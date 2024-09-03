559c02e7580bc7f29e61ecc5b9777cbc07bd9ba8
Phuc Nguyen <trongphuc1321@trongphuc1321.onmicrosoft.com>
2024-09-04 00:05:00 +0700

    TrongPhuccommit

---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PPKBeverageManagement.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
