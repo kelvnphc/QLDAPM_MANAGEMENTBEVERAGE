7d2e5ad30f7ffd44e0c76cdcef5ed043475b5cc5
Phuc Nguyen <trongphuc1321@trongphuc1321.onmicrosoft.com>
2024-09-15 17:53:08 +0700

    TrPhuc commitChartJs

---
+++ wwwroot/js/JavaScript.js
@@ -0,0 +1,24 @@
+﻿
+
+function drawChartTheoSoLuong(ctx, labels, data, title) {
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: title,
+                data: data,
+                borderWidth: 1,
+                backgroundColor: ['red', 'green', 'blue', 'gold', 'brown', 'orange']
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
